5fb4749f9f246630ce6878c33799cab7d010f169
Joel <joelmathew681@gmail.com>
2024-09-03 09:58:04 +0000

    Add intial application

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`johnwick programmer Info Security started on port ${PORT}`);
+});
+
+app.use(helmet.hidePoweredBy());
 
 
 
